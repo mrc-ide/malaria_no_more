f355137a88ee54a32679960e9aedeb79917fac2c
Hillary Topazian <80535782+htopazian@users.noreply.github.com>
2024-06-26 12:09:46 +0100

    Create data_and_libraries.R

---
+++ data_and_libraries.R
@@ -0,0 +1,15 @@
+# Libraries --------------------------------------------------------------------
+
+# Packages ----
+library(tidyverse)
+library(data.table)
+library(geodata)    # GADM
+library(sf)
+library(terra)
+
+# HPC
+library(hipercow) # for computer cluster: https://mrc-ide.github.io/hipercow/articles/hipercow.html
+
+# Plotting
+library(patchwork)
+library(MetBrewer) # devtools::install_github('BlakeRMills/MetBrewer')
